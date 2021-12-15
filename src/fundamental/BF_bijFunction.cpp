a265950e8b01c097a34b3c8af87ca98121aa3a3f
maticivan <maticivan@gmail.com>
2021-12-15 11:12:26 -0500

    Update BF_bijFunction.cpp

--- src/fundamental/BF_bijFunction.cpp
+++ src/fundamental/BF_bijFunction.cpp
@@ -227,7 +227,7 @@ namespace BF{
     tmp<<d;
     return tmp.str();
   }
-  std::string doubleToString(const double &d,const long &precision=12){
+  std::string doubleToString(const double &d,const long &precision=10){
     return eraseTrailingZeros(double_to_string_withPrecision(d,precision));
   }
 
