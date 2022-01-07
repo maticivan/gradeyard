b2534a16a267108a83f538c1c6166c183324600a
maticivan <maticivan@gmail.com>
2022-01-07 03:47:03 -0500

    Update CAGI_codeAutoGrader.cpp

--- src/interface/CAGI_codeAutoGrader.cpp
+++ src/interface/CAGI_codeAutoGrader.cpp
@@ -18,11 +18,16 @@
 #ifndef _INCL_CAGI_CodeAutoGrader_CPP
 #define _INCL_CAGI_CodeAutoGrader_CPP
 namespace CAGI{
+  struct GROneTestCase{
+  public:
+    std::string result, input, output, correct;
+  };
   struct GradingResult{
   public:
     double numericScore;
     std::string score;
-    std::string comment;
+    std::string comment,shorterComment;
+    std::vector<GROneTestCase> testCasesRes;
   };
   struct DockerCodeCounter{
   public:
@@ -38,12 +43,12 @@ namespace CAGI{
     }
     return allCodes[sz-1];
   }
-  RTI::CodeAutoGraderInfo getAutoGraderCodeData(const std::string &rawTxt, const std::string & offSolution, const std::string & userSolution, const std::string & maxPoints){
+  RTI::CodeAutoGraderInfo getAutoGraderCodeData(const std::string &agParameters, const std::string & offSolution, const std::string & userSolution, const std::string & maxPoints){
     RTI::CodeAutoGraderInfo cInfo;
     cInfo.officialSource=lastCode(offSolution);
     cInfo.userSource=lastCode(userSolution);
     std::pair<std::string,int> allD; long pos;
-    pos=0;allD=SF::extract(rawTxt,pos,"_language_","_/language_");
+    pos=0;allD=SF::extract(agParameters,pos,"_language_","_/language_");
     cInfo.language="none";
     if(allD.second==1){
       cInfo.language=allD.first;
@@ -55,19 +60,34 @@ namespace CAGI{
       }
     }
     cInfo.codeToEmbed="";
-    pos=0;allD=SF::extract(rawTxt,pos,"_embedIntoCode_","_/embedIntoCode_");
+    pos=0;allD=SF::extract(agParameters,pos,"_embedIntoCode_","_/embedIntoCode_");
     if(allD.second==1){
       cInfo.codeToEmbed=allD.first;
     }
+    cInfo.messageDidNotCompile="Code did not compile";
+    pos=0;allD=SF::extract(agParameters,pos,"_compilerErrorMessage_","_/compilerErrorMessage_");
+    if(allD.second==1){
+      cInfo.messageDidNotCompile=allD.first;
+    }
     cInfo.inputTestCases.resize(0);
-    pos=0;allD=SF::extract(rawTxt,pos,"_testCases_","_/testCases_");
+    pos=0;allD=SF::extract(agParameters,pos,"_testCases_","_/testCases_");
     if(allD.second==1){
       cInfo.inputTestCases=SF::stringToVector(allD.first,"_n*_","_/n*_");
     }
+    cInfo.publicTestCases.resize(0);
+    pos=0;allD=SF::extract(agParameters,pos,"_publicTestCases_","_/publicTestCases_");
+    if(allD.second==1){
+      cInfo.publicTestCases=SF::stringToVector(allD.first,"_n*_","_/n*_");
+    }
     cInfo.includes.resize(0);
-    pos=0;allD=SF::extract(rawTxt,pos,"_includes_","_/includes_");
+    pos=0;allD=SF::extract(agParameters,pos,"_includes_","_/includes_");
     if(allD.second==1){
       cInfo.includes=SF::stringToVector(allD.first,"_n*_","_/n*_");
+    }
+    cInfo.forbiddenStrs.resize(0);
+    pos=0;allD=SF::extract(agParameters,pos,"_forbidden_","_/forbidden_");
+    if(allD.second==1){
+      cInfo.forbiddenStrs=SF::stringToVector(allD.first,"_n*_","_/n*_");
     }
     cInfo.pointsTestCases.resize(0);
     long numTestCases=cInfo.inputTestCases.size();
@@ -79,7 +99,7 @@ namespace CAGI{
         cInfo.pointsTestCases[i]=maxScore / static_cast<double>(numTestCases);
         scoreByNow+=cInfo.pointsTestCases[i];
       }
-      pos=0;allD=SF::extract(rawTxt,pos,"_revealTestCasesAfterGrading_","_/revealTestCasesAfterGrading_");
+      pos=0;allD=SF::extract(agParameters,pos,"_revealTestCasesAfterGrading_","_/revealTestCasesAfterGrading_");
       if(allD.second==0){
         cInfo.revealTestCasesAfterGrading.resize(numTestCases);
         for(long i=0;i<numTestCases;++i){
@@ -89,7 +109,7 @@ namespace CAGI{
       else{
         cInfo.revealTestCasesAfterGrading=SF::stringToVector(allD.first,"_n*_","_/n*_");
       }
-      pos=0;allD=SF::extract(rawTxt,pos,"_pointsTestCases_","_/pointsTestCases_");
+      pos=0;allD=SF::extract(agParameters,pos,"_pointsTestCases_","_/pointsTestCases_");
       if(allD.second==1){
         std::vector<std::string> ptsTestCases = SF::stringToVector(allD.first,"_n*_","_/n*_");
         if(ptsTestCases.size()==numTestCases){
@@ -105,6 +125,21 @@ namespace CAGI{
         for(long i=0;i<numTestCases;++i){
           cInfo.pointsTestCases[i] *= rescale;
         }
+      }
+    }
+    cInfo.publicPointsTestCases.resize(0);
+    long pNumTC=cInfo.publicTestCases.size();
+    if(pNumTC>0){
+      cInfo.publicPointsTestCases.resize(pNumTC);
+      double maxScore=100.0;
+      double scoreByNow=0.0;
+      for(long i=0;i<pNumTC;++i){
+        cInfo.publicPointsTestCases[i]=maxScore / static_cast<double>(pNumTC);
+        scoreByNow+=cInfo.publicPointsTestCases[i];
+      }
+      cInfo.publicRevealTestCasesAfterGrading.resize(pNumTC);
+      for(long i=0;i<pNumTC;++i){
+        cInfo.publicRevealTestCasesAfterGrading[i]="yes";
       }
     }
     std::string testEmbed=SF::findAndReplace(cInfo.codeToEmbed,"_*embedHere*_","");
@@ -128,9 +163,9 @@ namespace CAGI{
     }
     return 0;
   }
-  GradingResult calculateScore(const std::vector<std::string> &officialOutput, const std::vector<std::string> &userOutput, const std::vector<double> &pts, const std::vector<std::string> & inputTestCases, const std::vector<std::string> &revealAfter){
+  GradingResult calculateScore(const std::vector<std::string> &officialOutput, const std::vector<std::string> &userOutput, const std::vector<double> &pts, const std::vector<std::string> & inputTestCases, const std::vector<std::string> &revealAfter, const std::string & mDNCompile){
     GradingResult res;
-    res.comment="";
+    res.comment="";res.shorterComment="";
     res.numericScore=-100.0;
     res.score="badScore";
     long sz=officialOutput.size();
@@ -140,6 +175,8 @@ namespace CAGI{
     if(sz!=pts.size()){
       return res;
     }
+    res.testCasesRes.resize(sz);
+    GROneTestCase tcRes;
     double total=0.0;
     long indicatorOfficialSolutionBad=0;
     long indicatorDidNotCompile=0;
@@ -155,39 +192,44 @@ namespace CAGI{
         indicatorOfficialSolutionBad=1;
       }
       res.comment+="\nTest case "+std::to_string(i+1)+": ";
+      tcRes.result="failure";
       if( sufficientlyEqual(officialOutput[i],userOutput[i])==1 ){
         total+=pts[i];
-        res.comment+="success";
+        tcRes.result="success";
       }
-      else{
-        res.comment+="failure";
-      }
+      res.comment+=tcRes.result;
+      tcRes.input="";tcRes.output="";tcRes.correct="";
       if(revealAfter[i]=="yes"){
         res.comment+="\n  Input: "+inputTestCases[i];
         res.comment+="\n Output: "+userOutput[i];
         res.comment+="\nCorrect: "+officialOutput[i]+"\n";
+        tcRes.input=inputTestCases[i];tcRes.output=userOutput[i];tcRes.correct=officialOutput[i];
       }
       if(userOutput[i] != SF::findAndReplace(userOutput[i],GF::GL_errorDidNotCompile,"")){
         indicatorDidNotCompile=1;
       }
+      res.testCasesRes[i]=tcRes;
       ++i;
     }
     if(indicatorOfficialSolutionBad==1){
       total=-100.0;
       res.comment="\nProblem with official solution.";
+      res.testCasesRes.resize(0);
     }
     if(indicatorDidNotCompile==1){
-      res.comment="\nCode did not compile.";
+      res.comment="\n"+mDNCompile;
+      res.testCasesRes.resize(0);
     }
     res.score=BF::eraseTrailingZeros(std::to_string(total));
     res.numericScore=total;
+    res.shorterComment=res.comment;
     res.comment="_code_AUTOGRADER COMMENT BEGIN"+res.comment+"\nAUTOGRADER COMMENT END_/code_";
     if(MWII::GL_WI.getDefaultWebText("ShowAutoGraderComment")=="no"){
       res.comment="";
     }
     return res;
   }
-  int codeAutoGradeAndUpdateMap(const PSDI::SessionData & _psd, std::map<std::string,GradingResult>& oldScores, const std::map<std::string,RTI::CodeAutoGraderInfo> & delayedAG){
+  int codeAutoGradeAndUpdateMap(const PSDI::SessionData & _psd, std::map<std::string,GradingResult>& oldScores, const std::map<std::string,RTI::CodeAutoGraderInfo> & delayedAG, const std::string & testCasesType){
     std::map<std::string,RTI::CodeAutoGraderInfo>::const_iterator it,itE;
     long numCodes=delayedAG.size();
     if(numCodes<1){
@@ -195,7 +237,9 @@ namespace CAGI{
     }
     std::vector<std::string> sources;
     std::vector<std::string> languages;
+    std::vector<std::string> mDidNotCompile;
     std::vector<std::vector<std::string> >includes;
+    std::vector<std::vector<std::string> >forbiddenStrs;
     std::vector<std::vector<std::string> >inputTestCases;
     std::vector<std::vector<std::string> >revealTCAfterGrading;
     std::vector<std::vector<double> > scores;
@@ -203,7 +247,9 @@ namespace CAGI{
     long twoNC=2*numCodes;
     sources.resize(twoNC);
     languages.resize(twoNC);
+    mDidNotCompile.resize(twoNC);
     includes.resize(twoNC);
+    forbiddenStrs.resize(twoNC);
     inputTestCases.resize(twoNC);
     revealTCAfterGrading.resize(twoNC);
     labels.resize(twoNC);
@@ -215,22 +261,40 @@ namespace CAGI{
       sources[i]=(it->second).userSource;
       languages[i]=(it->second).language;
       includes[i]=(it->second).includes;
-      inputTestCases[i]=(it->second).inputTestCases;
-      revealTCAfterGrading[i]=(it->second).revealTestCasesAfterGrading;
-      scores[i]=(it->second).pointsTestCases;
+      forbiddenStrs[i]=(it->second).forbiddenStrs;
+      if(testCasesType=="publicTestCases"){
+        inputTestCases[i]=(it->second).publicTestCases;
+        revealTCAfterGrading[i]=(it->second).publicRevealTestCasesAfterGrading;
+        scores[i]=(it->second).publicPointsTestCases;
+      }
+      else{
+        inputTestCases[i]=(it->second).inputTestCases;
+        revealTCAfterGrading[i]=(it->second).revealTestCasesAfterGrading;
+        scores[i]=(it->second).pointsTestCases;
+      }
+      mDidNotCompile[i]=(it->second).messageDidNotCompile;
       labels[i]=it->first;
       ++i;
       sources[i]=(it->second).officialSource;
       languages[i]=(it->second).language;
       includes[i]=(it->second).includes;
-      inputTestCases[i]=(it->second).inputTestCases;
-      revealTCAfterGrading[i]=(it->second).revealTestCasesAfterGrading;
-      scores[i]=(it->second).pointsTestCases;
+      forbiddenStrs[i]=(it->second).forbiddenStrs;
+      if(testCasesType=="publicTestCases"){
+        inputTestCases[i]=(it->second).publicTestCases;
+        revealTCAfterGrading[i]=(it->second).publicRevealTestCasesAfterGrading;
+        scores[i]=(it->second).publicPointsTestCases;
+      }
+      else{
+        inputTestCases[i]=(it->second).inputTestCases;
+        revealTCAfterGrading[i]=(it->second).revealTestCasesAfterGrading;
+        scores[i]=(it->second).pointsTestCases;
+      }
+      mDidNotCompile[i]=(it->second).messageDidNotCompile;
       labels[i]=it->first;
       ++i;
       ++it;
     }
-    std::pair<std::vector<std::vector<std::string> >,int> aGOutput=DCEI::executePrograms(_psd,sources,languages,includes,inputTestCases);
+    std::pair<std::vector<std::vector<std::string> >,int> aGOutput=DCEI::executePrograms(_psd,sources,languages,includes,forbiddenStrs,inputTestCases);
     if(aGOutput.second==0){
       return 0;
     }
@@ -240,7 +304,7 @@ namespace CAGI{
     i=0;
     GradingResult score1Problem;
     while(i<numCodes){
-      score1Problem=calculateScore(aGOutput.first[2*i+1],aGOutput.first[2*i],scores[2*i],inputTestCases[2*i],revealTCAfterGrading[2*i]);
+      score1Problem=calculateScore(aGOutput.first[2*i+1],aGOutput.first[2*i],scores[2*i],inputTestCases[2*i],revealTCAfterGrading[2*i],mDidNotCompile[2*i]);
       if(score1Problem.score!="badScore"){
         oldScores[labels[2*i]]=score1Problem;
       }
@@ -281,17 +345,18 @@ namespace CAGI{
     fR+="</ul>";
     return fR;
   }
-  std::string executionResult(const PSDI::SessionData & _psd, const std::string & rawAgr, const std::string & offSolution){
-    RTI::CodeAutoGraderInfo cInfo=getAutoGraderCodeData(rawAgr,offSolution,"","100");
+  std::string executionResult(const PSDI::SessionData & _psd, const std::string & agrParameters, const std::string & offSolution){
+    RTI::CodeAutoGraderInfo cInfo=getAutoGraderCodeData(agrParameters,offSolution,"","100");
     std::vector<std::string> sources,languages;
-    std::vector<std::vector<std::string> > includes, inputTestCases;
+    std::vector<std::vector<std::string> > includes, forbiddenStrs, inputTestCases;
     sources.resize(1);languages.resize(1);
-    includes.resize(1);inputTestCases.resize(1);
+    includes.resize(1);forbiddenStrs.resize(1);inputTestCases.resize(1);
     sources[0]=cInfo.officialSource;
     languages[0]=cInfo.language;
     includes[0]=cInfo.includes;
+    forbiddenStrs[0]=cInfo.forbiddenStrs;
     inputTestCases[0]=cInfo.inputTestCases;
-    std::pair<std::vector<std::vector<std::string> >,int> aGOutput=DCEI::executePrograms(_psd, sources,languages,includes,inputTestCases);
+    std::pair<std::vector<std::vector<std::string> >,int> aGOutput=DCEI::executePrograms(_psd, sources,languages,includes,forbiddenStrs,inputTestCases);
     std::string fR="<h4>Code execution on test cases</h4>";
     if((aGOutput.second==0)||(aGOutput.first.size()!=1)){
       fR+=wrongLengthsOfVectors();
