e845ac8c84bbebfa503bd23ea028ec73aef06421
maticivan <maticivan@gmail.com>
2022-02-23 16:02:33 -0500

    Update ATMD_abstractTextManagement.cpp

--- src/db/ATMD_abstractTextManagement.cpp
+++ src/db/ATMD_abstractTextManagement.cpp
@@ -15,37 +15,26 @@
 //    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
 //    along with this program.  If not, see https://www.gnu.org/licenses/.
 
-
 #ifndef _INCL_ABSTRACTTEXTMANAGEMENT_CPP
 #define _INCL_ABSTRACTTEXTMANAGEMENT_CPP
 namespace ATMD{
 
   class Text:public AMD::AbstractManager{
   protected:
-
-
     std::string textName;
     std::string ow_dbs_key0="id";
     std::string ow_dbs_key1="name";
     std::string ow_dbsMFInd="mainText";
     std::string ow_cntrName="textsCntr";
-
-
     long ow_primeIndex0=9;
     long ow_primeIndex1=20;
     long ow_permutInd=8;
     long ow_shift=76442;
-
-
   public:
     Text(const std::string & ="!*!", const std::string & ="!*!");
     std::string getTextName() const;
     std::string getKey1() const;
     void setKey1(const std::string &);
-
-
-
-
     int createText(const std::string & = "!*!");
     // arguments: 1) textName
     //    If no argument is present, the text name will be assigned
@@ -53,16 +42,10 @@ namespace ATMD{
     // returns :
     // 1 - success
     // 0 - textName already used
-
     int textNameExists(const std::string &_tn) const;
-
-
     int setFromTextName(const std::string &);
-
-
   };
   int Text::setFromTextName(const std::string & _tn){
-
     std::vector<std::string> k,v;
     k.resize(1);v.resize(1);
     k[0]=dbs_key1;
@@ -76,17 +59,12 @@ namespace ATMD{
     std::string _iId=searchRes[0].first[0];
     internalVectorNumber=getInternalNumberFromInternalId(_iId);
     textName=_tn;
-
     textData=searchRes[0].second;
-
     existenceEstablishedBefore=1;
     return 1;
   }
-
-
   Text::Text(const std::string & _tn, const std::string &  _intTId){
   }
-
   std::string Text::getTextName() const{
     return textName;
   }
@@ -96,17 +74,12 @@ namespace ATMD{
   void Text::setKey1(const std::string & _k){
     textName=_k;
   }
-
-
-
-
   int Text::textNameExists(const std::string &_tn) const{
     Text tmp;
     int exists=tmp.setFromTextName(_tn);
     return exists;
   }
   int Text::createText(const std::string & _tn){
-
     std::string tn="!*!";
     if(_tn!="!*!"){
       //check whether _tn already exists. If it does, exit with 0
@@ -115,8 +88,6 @@ namespace ATMD{
       }
       tn=_tn;
     }
-
-
     long textIdIsFree=0;
     std::string textId;
     std::vector<std::string> k,v;
@@ -151,6 +122,5 @@ namespace ATMD{
     existenceEstablishedBefore=1;
     return 1;
   }
-
 }
 #endif
