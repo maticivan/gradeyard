d1f6d3de20b1943ad4b6b7adedf627a1794acb1c
maticivan <maticivan@gmail.com>
2022-06-21 23:33:16 -0400

    Add files via upload

---
+++ src/fundamental/SAIOF_setupAndIO.cpp
@@ -0,0 +1,94 @@
+//    GradeYard learning management system
+//
+//    Copyright (C) 2022 Ivan Matic, https://gradeyard.com
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+//
+//    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+//    along with this program.  If not, see https://www.gnu.org/licenses/.
+
+#ifndef _INCL_SAIOF_CPP
+#define _INCL_SAIOF_CPP
+
+
+#include "SSF_sortedSeq.cpp"
+#include "BF_bijFunction.cpp"
+#include "TMF_timer.cpp"
+#include "DEBMF_debuggingMessages.cpp"
+#include "HENCF_hashingEnc.cpp"
+#include "VSF_vectorSearches.cpp"
+#include "GF_globalInits.cpp"
+#include "RNDF_randomCodesAndPermutations.cpp"
+#include "SF_strings.cpp"
+#include "HSF_html_strings.cpp"
+#include "IOF_io.cpp"
+#include "ENCF_encryption.cpp"
+#include "LNF_labelsAndNumbers.cpp"
+#include "LMF_latexManipulation.cpp"
+#include "HCBF_htmlTagStorageForCodeBox.cpp"
+#include "SPREPF_statPreparation.cpp"
+#include "PASF_patternAnalysisStrings.cpp"
+#include "DERF_derangements.cpp"
+
+namespace SAIOF{
+  int match(const std::string & st1, const std::string & st2,  long direction=1){
+    long sz1=st1.length(); long sz2=st2.length();
+    if(sz1 < sz2){
+      return 0;
+    }
+    if(direction==0){direction=1;}
+    long begin1=0;long begin2=0;
+    long end2=sz2;
+    if(direction<0){
+      begin1=sz1-1;begin2=sz2-1;end2=-1;
+    }
+    int stillGood=1;
+    while(stillGood&&(begin2!=end2)){
+      if(st1[begin1]!=st2[begin2]){stillGood=0;}
+      begin1+=direction;
+      begin2+=direction;
+    }
+    return stillGood;
+  }
+  int startMatch(const std::string &st1, const std::string &st2){
+    return match(st1,st2,1);
+  }
+  int endMatch(const std::string &st1, const std::string &st2){
+    return match(st1,st2,-1);
+  }
+  int looksLikeSetupFile(const std::string &st,const std::string &start="st", const std::string &end="e.txt"){
+    return startMatch(st,start)*endMatch(st,end);
+  }
+  std::string get_GL_MAIN_SETUP_FILE_NAME(){ 
+    std::vector<std::string> allFiles=IOF::listFiles(".");
+    long db_sz=allFiles.size();
+    long i=0;
+    std::string found="";
+    while((i<db_sz)&&(found=="")){
+      allFiles[i]=SF::findAndReplace(allFiles[i],"./","");
+      if(looksLikeSetupFile(allFiles[i],"st","e.txt")){
+        found=allFiles[i];
+      }
+      ++i;
+    }
+    if(found!=""){return found;}
+    i=0;
+    while((i<db_sz)&&(found=="")){
+      if(looksLikeSetupFile(allFiles[i],"setup",".txt")){
+        found=allFiles[i];
+      }
+      ++i;
+    }
+    if(found!=""){return found;}
+    return "notFoundSetup.txt";
+  }
+}
+#endif
