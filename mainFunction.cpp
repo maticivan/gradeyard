d498e0698f92cacac42d25d84b9608e6a42ed13e
maticivan <maticivan@gmail.com>
2022-08-24 11:30:46 -0400

    Update mainFunction.cpp

--- mainFunction.cpp
+++ mainFunction.cpp
@@ -18,7 +18,7 @@
 #include "src/fundamental/SHF_standardHeaders.cpp"
 #include "src/fundamental/SAIOF_setupAndIO.cpp"
 std::string GL_MAIN_SETUP_FILE_NAME=SAIOF::get_GL_MAIN_SETUP_FILE_NAME();
-std::string GL_VERSION="1.002.2022.06.22";
+std::string GL_VERSION="1.003.2022.08.24";
 #include "src/fundamental/MTF_mainTable.cpp"
 #include "src/db/DD_databases.cpp"
 #include "src/graphs/MGG_mainGraphs.cpp"
