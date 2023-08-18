611b27ac5fba3ab835c1c05ef3fc4f740f69a2c8
maticivan <maticivan@gmail.com>
2023-08-18 00:49:03 -0400

    Update 2.0.0 mainFunction.cpp
    Changed the rules how the databases are written in raw files. This update 2.0.0 has improved the speed of the program. The versions 2.*.* do not have compatible databases with the versions 1.*.*

--- mainFunction.cpp
+++ mainFunction.cpp
@@ -18,7 +18,7 @@
 #include "src/fundamental/SHF_standardHeaders.cpp"
 #include "src/fundamental/SAIOF_setupAndIO.cpp"
 std::string GL_MAIN_SETUP_FILE_NAME=SAIOF::get_GL_MAIN_SETUP_FILE_NAME();
-std::string GL_VERSION="1.17.17 (2023/08/14)";
+std::string GL_VERSION="2.0.0 (2023/08/17)";
 #include "src/fundamental/MTF_mainTable.cpp"
 #include "src/db/DD_databases.cpp"
 #include "src/graphs/MGG_mainGraphs.cpp"
