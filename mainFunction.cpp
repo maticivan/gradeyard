028f4ed55aae64dbd700bda959d2d45edf5dbf27
maticivan <maticivan@gmail.com>
2023-09-19 14:38:19 -0400

    Update 2.1.16 to mainFunction.cpp
    There was a bug in the previous version that caused certain math inequalities to be improperly displayed. The bug is corrected by placing an extra space after inequality signs, if there was no space there before.

--- mainFunction.cpp
+++ mainFunction.cpp
@@ -18,7 +18,7 @@
 #include "src/fundamental/SHF_standardHeaders.cpp"
 #include "src/fundamental/SAIOF_setupAndIO.cpp"
 std::string GL_MAIN_SETUP_FILE_NAME=SAIOF::get_GL_MAIN_SETUP_FILE_NAME();
-std::string GL_VERSION="2.1.12 (2023/09/07)";
+std::string GL_VERSION="2.1.16 (2023/09/19)";
 #include "src/fundamental/MTF_mainTable.cpp"
 #include "src/db/DD_databases.cpp"
 #include "src/graphs/MGG_mainGraphs.cpp"
