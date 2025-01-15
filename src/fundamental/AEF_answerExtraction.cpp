3c73cc7f8a38404f3fcf700219cc97b870822bd8
maticivan <maticivan@gmail.com>
2025-01-15 15:46:46 -0500

    Update 2.7.1 to AEF_answerExtraction.cpp

--- src/fundamental/AEF_answerExtraction.cpp
+++ src/fundamental/AEF_answerExtraction.cpp
@@ -1,6 +1,6 @@
 //    GradeYard learning management system
 //
-//    Copyright (C) 2023 Ivan Matic, https://gradeyard.com
+//    Copyright (C) 2025 Ivan Matic, https://gradeyard.com
 //
 //    This program is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as published by
@@ -28,6 +28,10 @@ namespace AEF{
     replMap["\\begin{eqnarray}"]=" $";
     replMap["\\end{eqnarray*}"]="$ ";
     replMap["\\end{eqnarray}"]="$ ";
+    replMap["\\begin{align*}"]=" $";
+    replMap["\\begin{align}"]=" $";
+    replMap["\\end{align*}"]="$ ";
+    replMap["\\end{align}"]="$ ";
     replMap["\\begin{equation*}"]=" $";
     replMap["\\begin{equation}"]=" $";
     replMap["\\end{equation*}"]="$ ";
