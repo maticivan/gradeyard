b018747c3a60fe91cfed30715fd71e6fdf434017
maticivan <maticivan@gmail.com>
2025-09-25 16:13:29 -0400

    Update 2.10.2 to mainFunction.cpp
    Update to C++ tables. The input form, output html and latex, and the source can have a repetitive loop that are unrolled before displaying and docker compiling.

--- mainFunction.cpp
+++ mainFunction.cpp
@@ -18,7 +18,7 @@
 #include "src/fundamental/SHF_standardHeaders.cpp"
 #include "src/fundamental/SAIOF_setupAndIO.cpp"
 std::string GL_MAIN_SETUP_FILE_NAME=SAIOF::get_GL_MAIN_SETUP_FILE_NAME();
-std::string GL_VERSION="2.10.1 (2025/08/22)";
+std::string GL_VERSION="2.10.2 (2025/09/25)";
 #include "src/fundamental/MTF_mainTable.cpp"
 #include "src/db/DD_databases.cpp"
 #include "src/graphs/MGG_mainGraphs.cpp"
