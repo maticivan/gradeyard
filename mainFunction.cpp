5b7608e8e14dfac05e9b0ebfda1cb74f72dc0f2d
maticivan <maticivan@gmail.com>
2025-12-22 14:08:29 -0500

    Update 2.17.2 to mainFunction.cpp
    Improved WCAG functions.

--- mainFunction.cpp
+++ mainFunction.cpp
@@ -18,7 +18,7 @@
 #include "src/fundamental/SHF_standardHeaders.cpp"
 #include "src/fundamental/SAIOF_setupAndIO.cpp"
 std::string GL_MAIN_SETUP_FILE_NAME=SAIOF::get_GL_MAIN_SETUP_FILE_NAME();
-std::string GL_VERSION="2.17.1 (2025/12/19)";
+std::string GL_VERSION="2.17.2 (2025/12/22)";
 #include "src/fundamental/MTF_mainTable.cpp"
 #include "src/db/DD_databases.cpp"
 #include "src/graphs/MGG_mainGraphs.cpp"
