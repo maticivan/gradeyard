5339e888b470abf4b63990bf4341b06e01701c33
maticivan <maticivan@gmail.com>
2026-03-06 20:31:35 -0500

    Update 2.20.6

--- src/fundamental/DISPPF_displayPreparations.cpp
+++ src/fundamental/DISPPF_displayPreparations.cpp
@@ -1,6 +1,6 @@
 //    GradeYard learning management system
 //
-//    Copyright (C) 2025 Ivan Matic, https://gradeyard.com
+//    Copyright (C) 2026 Ivan Matic, https://gradeyard.com
 //
 //    This program is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as published by
@@ -18,19 +18,46 @@
 #ifndef _INCL_DISPLAYPREPARATIONS_CPP
 #define _INCL_DISPLAYPREPARATIONS_CPP
 namespace DISPPF{
-  void dollarsToLatexSymbols(std::string & output,const long &singleDollarsAlso  =1){
-    std::string azm=GF::GL_HIDING_STRING_DISPPF01;
-    std::vector<std::string> w2El;
-    w2El.resize(2);
-    output=SF::findAndReplace(output,"\\$",azm);
-    w2El[0]="\\[";w2El[1]="\\]";
-    output=SF::findAndReplace(output,"$$",w2El);
-    if(singleDollarsAlso==1){
-      w2El[0]="\\(";w2El[1]="\\)";
-      output=SF::findAndReplace(output,"$",w2El);
+std::string openOrCloseTag(long& toggleCounter,
+                            const std::string& openTag,
+                            const std::string& closeTag){
+    ++toggleCounter;
+    if(toggleCounter % 2 == 1){
+        return openTag;
     }
-    output=SF::findAndReplace(output,azm,"\\$");
-  }
+    return closeTag;
+}
+std::string convertDollarsToLatexDelimiters(const std::string& in){
+    std::string out;
+    long inlineMathToggle  = 0;
+    long displayMathToggle = 0;
+    const std::string inlineOpen  = "\\(";
+    const std::string inlineClose = "\\)";
+    const std::string displayOpen  = "\\[";
+    const std::string displayClose = "\\]";
+    long i = 0;
+    while(i < in.size()){
+        if(in[i] == '$'){
+            bool escapedDollar = (i > 0) && (in[i-1] == '\\');
+            bool displayMath   = (i < in.size()-1) && (in[i+1] == '$');
+            if(escapedDollar){
+                out += '$';
+            }
+            else if(displayMath){
+                out += openOrCloseTag(displayMathToggle, displayOpen, displayClose);
+                ++i;
+            }
+            else{
+                out += openOrCloseTag(inlineMathToggle, inlineOpen, inlineClose);
+            }
+        }
+        else{
+            out += in[i];
+        }
+        ++i;
+    }
+    return out;
+}
   int removeBoxCodeToSafety(PTKF::PlainTextKeeper &kc, std::string &t,const std::string & s_B="_codeBox_",const std::string & s_E="_/codeBox_"){
     //returns 1 if everything is removed properly
     // returns 0 if there is an open tag or a tag inside a tag
@@ -55,13 +82,13 @@ namespace DISPPF{
   public:
     std::vector<std::string> formattingTags;
     std::vector<std::string> toEliminateFromSimpleTexts;
-    std::vector<std::string> alakazams;
+    //std::vector<std::string> alakazams;
     std::map<std::string,std::string> tagRemovalMap;
     HTML_Tags();
   };
   HTML_Tags::HTML_Tags(){
     long i;
-    formattingTags.resize(25);
+    formattingTags.resize(26);
     i=-1;
     ++i;formattingTags[i]="<i>";
     ++i;formattingTags[i]="</i>";
@@ -88,11 +115,7 @@ namespace DISPPF{
     ++i;formattingTags[i]="<h5>";
     ++i;formattingTags[i]="</h5>";
     ++i;formattingTags[i]="<br>";
-    long sz=formattingTags.size();
-    alakazams.resize(sz);
-    for(long i=0;i<sz;++i){
-      alakazams[i]=azm+std::to_string(i)+"e|";
-    }
+    ++i;formattingTags[i]="</a>";
     toEliminateFromSimpleTexts.resize(21);
     i=-1;
     ++i;toEliminateFromSimpleTexts[i]="<div>";
@@ -124,63 +147,6 @@ namespace DISPPF{
     }
   }
   HTML_Tags GL_HTML_Tags;
-  std::string treatHideReveal(const std::string & _input){
-    std::string output=_input;
-    long pos,posToSave;
-    std::pair<std::string,int> allD;
-    std::string revT,hideT,text,nt,oldText;
-    long counter=0;std::string counterSt;
-    pos=0;allD=SF::extract(output,pos,"_hideReveal_","_/hideReveal_");
-    while(allD.second==1){
-      text=allD.first;
-      oldText="_hideReveal_"+text+"_/hideReveal_";
-      posToSave=pos;
-      posToSave-=text.size();
-      if(posToSave<0){
-        posToSave=0;
-      }
-      pos=0;allD=SF::extract(text,pos,"_hideTitle_","_/hideTitle_");
-      hideT="Hide";
-      if(allD.second==1){
-        hideT=allD.first;
-      }
-      pos=0;allD=SF::extractAndReplace(text,pos,"_hideTitle_","_/hideTitle_");
-      if(allD.second==1){
-        text=allD.first;
-      }
-      pos=0;allD=SF::extract(text,pos,"_revealTitle_","_/revealTitle_");
-      revT="Show";
-      if(allD.second==1){
-        revT=allD.first;
-      }
-      pos=0;allD=SF::extractAndReplace(text,pos,"_revealTitle_","_/revealTitle_");
-      if(allD.second==1){
-        text=allD.first;
-      }
-      counterSt=std::to_string(counter);
-      nt="<div id=\"someText3"+counterSt+"\">\n";
-      nt+="<a id=\"headerL3"+counterSt+"\" href=\"javascript:toggle(&#39;mCont3"+counterSt+"&#39;, ";
-        nt+="&#39;hDiv3"+counterSt+"&#39;, &#39;"+revT+"&#39;, &#39;"+hideT+"&#39; ) ;\" aria-label=\"Show hidden content ";
-      nt+=counterSt+"\">";
-      nt+="<button type=\"button\" class=\"btn btn-outline-dark btn-sm\">";
-      nt+="<span id=\"hDiv3";
-      nt+=counterSt+"\">"+revT+"</span></button></a>\n";
-      nt+="</div><div id=\"mCont3"+counterSt+"\" class=\"card\" style=\"background-color:#e9ebf3; display: none;\">";
-      nt+="<div class=\"card-body\">";
-      nt+=text+"</div></div>";
-      output=SF::findAndReplace(output,oldText,nt);
-      pos=posToSave;
-      pos=0;
-      allD=SF::extract(output,pos,"_hideReveal_","_/hideReveal_");
-      ++counter;
-    }
-    return output;
-  }
-  std::string formattingCommands(const std::string & _input){
-    std::string output=_input;
-    output=treatHideReveal(output);
-    return output;
-  }
   struct RequestsForSanitizer{
   public:
     long convertDollarsToLatex;
@@ -202,50 +168,22 @@ namespace DISPPF{
   // 1 - allow formatting tags.  WARNING -- not implemented fully yet.
 
   // 2 - allow all tags except for dangerous ones
-    PTKF::PlainTextKeeper keeperOfVerbatim("vb0b");
-    PTKF::PlainTextKeeper keeperOfCodesB("sc0b");
-    PTKF::PlainTextKeeper keeperOfBoxCodes("sc0bb");
-    PTKF::PlainTextKeeper keeperOfCodesS("sc0s");
-    PTKF::PlainTextKeeper keeperOfPresB("p0b");
-    PTKF::PlainTextKeeper keeperOfPresS("p0s");
-    std::string t=_t;
+      std::string dnchaB="_doNotChangeAlphabet*_";
+      std::string dnchaE="_/doNotChangeAlphabet*_";
+      if(GF::GL_Alphabet=="english"){
+        dnchaB=""; dnchaE="";
+      }
+      std::string t=_t;
+      PTKF::PlainTextKeeper keeperOfBoxCodes("sc0bb");
     std::string unsafe="Latex Error ";
     int indicatorSafety;
-    indicatorSafety=PTKF::removeToSafety(keeperOfVerbatim,t,"_verbatim_","_/verbatim_");
-    if((1-indicatorSafety)* (rs.exitWithErrorIfUnsafe)==1){
-      return unsafe+" 1";
-    }
-    indicatorSafety=PTKF::removeToSafety(keeperOfCodesB,t,"_code_","_/code_");
-    if((1-indicatorSafety)*(rs.exitWithErrorIfUnsafe)==1){
-      return unsafe+" 2";
-    }
-    indicatorSafety=PTKF::removeToSafety(keeperOfPresB,t,"<pre>","</pre>");
-    if((1-indicatorSafety)*(rs.exitWithErrorIfUnsafe)==1){
-      return unsafe+" 2";
-    }
-    indicatorSafety=PTKF::removeToSafety(keeperOfPresS,t,"<code>","</code>");
-    if((1-indicatorSafety)*(rs.exitWithErrorIfUnsafe)==1){
-      return unsafe+" 2";
-    }
     indicatorSafety=removeBoxCodeToSafety(keeperOfBoxCodes,t,"_codeBox_","_/codeBox_");
     if((1-indicatorSafety)*(rs.exitWithErrorIfUnsafe)==1){
       return unsafe+" 4";
     }
-    indicatorSafety=PTKF::removeToSafety(keeperOfCodesS,t,"_cde_","_/cde_");
-    if((1-indicatorSafety)*(rs.exitWithErrorIfUnsafe)==1){
-      return unsafe+" 3";
-    }
-    keeperOfVerbatim.treatCDE("_verbatim_","_/verbatim_");
-    keeperOfCodesB.treatCODE();
-    keeperOfCodesS.treatCDE();
-    keeperOfPresB.treatPre("<pre>","</pre>");
-    keeperOfPresS.treatPre("<code>","</code>");
     keeperOfBoxCodes.treatBoxCode();
     if(rs.convertDollarsToLatex==1){
-      std::vector<std::string>ws;
-      ws.resize(2);
-      ws[0]="1";ws[1]="2";
-      dollarsToLatexSymbols(t);
+        t=convertDollarsToLatexDelimiters(t);
     }
     std::string tForTesting1=t;
     tForTesting1=SF::findAndReplace(tForTesting1,"_/comment_"," _/comment_");
@@ -253,73 +191,115 @@ namespace DISPPF{
       return "unsafeInput";
     }
     t=MFRF::findAndReplace(t,GF::GL_DANGERS.veryBadStringsToBeErased);
-    PTKF::PlainTextKeeper mth1("m01");
-    indicatorSafety=PTKF::removeToSafety(mth1,t,"\\(","\\)");
-    if((1-indicatorSafety)*(rs.exitWithErrorIfUnsafe)==1){
-      return unsafe+" 4 ";
-    }
-    PTKF::PlainTextKeeper mth2("m02");
-    indicatorSafety=PTKF::removeToSafety(mth2,t,"\\[","\\]");
-    if((1-indicatorSafety)*(rs.exitWithErrorIfUnsafe)==1){
-      return unsafe+"5";
-    }
-    PTKF::PlainTextKeeper mth3("m03");
-    indicatorSafety=PTKF::removeToSafety(mth3,t,"\\begin{eqnarray*}","\\end{eqnarray*}");
-    if((1-indicatorSafety)*(rs.exitWithErrorIfUnsafe)==1){
-      return unsafe+"6";
-    }
-    PTKF::PlainTextKeeper mth4("m04");
-    indicatorSafety=PTKF::removeToSafety(mth4,t,"\\begin{eqnarray}","\\end{eqnarray}");
-    if((1-indicatorSafety)*(rs.exitWithErrorIfUnsafe)==1){
-      return unsafe+"7";
-    }
-    PTKF::PlainTextKeeper mth5("m05");
-    indicatorSafety=PTKF::removeToSafety(mth5,t,"\\begin{equation}","\\end{equation}");
-    if((1-indicatorSafety)*(rs.exitWithErrorIfUnsafe)==1){
-      return unsafe+"8";
-    }
-    PTKF::PlainTextKeeper mth6("m06");
-    indicatorSafety=PTKF::removeToSafety(mth6,t,"\\begin{align*}","\\end{align*}");
-    if((1-indicatorSafety)*(rs.exitWithErrorIfUnsafe)==1){
-      return unsafe+"9";
-    }
-    PTKF::PlainTextKeeper mth7("m07");
-    indicatorSafety=PTKF::removeToSafety(mth7,t,"\\begin{align}","\\end{align}");
-    if((1-indicatorSafety)*(rs.exitWithErrorIfUnsafe)==1){
-      return unsafe+"10";
-    }
-    mth7.treatMath();
-    mth6.treatMath();
-    mth5.treatMath();
-    mth4.treatMath();
-    mth3.treatMath();
-    mth2.treatMath();
-    mth1.treatMath();
-    if(rs.htmlTolerance<2){
-      if(rs.htmlTolerance==1){
-        t=MFRF::findAndReplace(t,GL_HTML_Tags.formattingTags,GL_HTML_Tags.alakazams);
+      std::vector<std::string> openTags, closeTags;
+      std::vector<std::string> openTagReplacements, closeTagReplacements;
+      std::vector<std::string> instructions;
+      long numTags=12,numMathTags=7;
+      if(rs.htmlTolerance<2){++numTags;}
+      openTags.resize(numTags);
+      closeTags.resize(numTags);
+      instructions.resize(numTags);
+      for(long i=0;i<numMathTags;++i){
+          instructions[i]="math";
       }
+      long tagCntr=numMathTags;
+      instructions[tagCntr]="pre";++tagCntr;
+      instructions[tagCntr]="code";++tagCntr;
+      instructions[tagCntr]="pre";++tagCntr;
+      instructions[tagCntr]="code";++tagCntr;
+      instructions[tagCntr]="code";++tagCntr;
+      if(rs.htmlTolerance<2){instructions[tagCntr]="aHref";++tagCntr;}
+      tagCntr=0;
+      openTags[tagCntr]="\\(";
+      closeTags[tagCntr]="\\)";
+      ++tagCntr;
+      openTags[tagCntr]="\\[";
+      closeTags[tagCntr]="\\]";
+      ++tagCntr;
+      openTags[tagCntr]="\\begin{eqnarray*}";
+      closeTags[tagCntr]="\\end{eqnarray*}";
+      ++tagCntr;
+      openTags[tagCntr]="\\begin{eqnarray}";
+      closeTags[tagCntr]="\\end{eqnarray}";
+      ++tagCntr;
+      openTags[tagCntr]="\\begin{equation}";
+      closeTags[tagCntr]="\\end{equation}";
+      ++tagCntr;
+      openTags[tagCntr]="\\begin{align*}";
+      closeTags[tagCntr]="\\end{align*}";
+      ++tagCntr;
+      openTags[tagCntr]="\\begin{align}";
+      closeTags[tagCntr]="\\end{align}";
+      ++tagCntr;
+      openTags[tagCntr]="<pre>";
+      closeTags[tagCntr]="</pre>";
+      ++tagCntr;
+      openTags[tagCntr]="<code>";
+      closeTags[tagCntr]="</code>";
+      ++tagCntr;
+      openTags[tagCntr]="_code_";
+      closeTags[tagCntr]="_/code_";
+      ++tagCntr;
+      openTags[tagCntr]="_cde_";
+      closeTags[tagCntr]="_/cde_";
+      ++tagCntr;
+      openTags[tagCntr]="_verbatim_";
+      closeTags[tagCntr]="_/verbatim_";
+      ++tagCntr;
+      openTagReplacements=openTags;
+      closeTagReplacements=closeTags;
+      tagCntr -= 3;
+      //This CMS may still contain texts from the very old CMS, and
+      //has to be robust enough to understand those texts.
+      //In those old texts, _code_ is what we now call <pre>
+      //and _cde_ is what we now call <code>
+      //Now we have openTagReplacements[tagCntr]=="_code_";
+      openTagReplacements[tagCntr]=dnchaB+"<pre>";
+      closeTagReplacements[tagCntr]="</pre>"+dnchaE;
+      ++tagCntr;
+      //Now we have openTagReplacements[tagCntr]=="_cde_";
+      openTagReplacements[tagCntr]=dnchaB+"<code>";
+      closeTagReplacements[tagCntr]="</code>"+dnchaE;
+      ++tagCntr;
+      //Now we have openTagReplacements[tagCntr]=="_verbatim_";
+      openTagReplacements[tagCntr]=dnchaB+"<code>";
+      closeTagReplacements[tagCntr]="</code>"+dnchaE;
+      ++tagCntr;
+      //Now we are back with tagCntr, we had tagCntr-=3, and then we had
+      // 3 instructions ++tagCntr;
+      if(rs.htmlTolerance<2){
+          openTags[tagCntr]="<a href=\"";
+          closeTags[tagCntr]="\">";
+          openTagReplacements[tagCntr]=openTags[tagCntr];
+          closeTagReplacements[tagCntr]=closeTags[tagCntr];
+          ++tagCntr;
+      }
+    std::map<std::string,std::string> tagRecoveryMap;
+    PTKF::hideTexts(t,tagRecoveryMap,
+                    openTags,
+                    closeTags,
+                    openTagReplacements,
+                    closeTagReplacements,
+                    instructions,
+                    GL_HTML_Tags.formattingTags,
+                    GF::GL_HIDING_STRING_DISPPF02,
+                    rs.htmlTolerance);
+      if(rs.htmlTolerance<2){
+          std::map<std::string,std::string> replMap;
+          replMap["<"]="\\(<\\)";
+          replMap[">"]="\\(>\\)";
+          t=MFRF::findAndReplace(t,replMap);
+      }
+      t=MFRF::findAndReplace(t,tagRecoveryMap);
+      t=PTKF::treatHideReveal(t);
       std::map<std::string,std::string> replMap;
-      replMap["<"]="\\(<\\)";
-      replMap[">"]="\\(>\\)";
+      replMap[PTKF::GL_hideRevealOpenH]="_hideReveal_";
+      replMap[PTKF::GL_hideRevealCloseH]="_/hideReveal_";
+      replMap[PTKF::GL_openHidden]="";
+      replMap[PTKF::GL_closeHidden]="";
+      replMap[PTKF::GL_instructionOpen]="";
+      replMap[PTKF::GL_instructionClose]="";
       t=MFRF::findAndReplace(t,replMap);
-      if(rs.htmlTolerance==1){
-        t=MFRF::findAndReplace(t,GL_HTML_Tags.alakazams,GL_HTML_Tags.formattingTags);
-      }
-    }
-    t=mth7.recover(t);
-    t=mth6.recover(t);
-    t=mth5.recover(t);
-    t=mth4.recover(t);
-    t=mth3.recover(t);
-    t=mth2.recover(t);
-    t=mth1.recover(t);
-    t=formattingCommands(t);
-    t=keeperOfCodesS.recover(t);
-    t=keeperOfPresS.recover(t);
-    t=keeperOfPresB.recover(t);
-    t=keeperOfCodesB.recover(t);
-    t=keeperOfVerbatim.recover(t);
     t=keeperOfBoxCodes.recover(t);
     return t;
   }
@@ -331,7 +311,7 @@ namespace DISPPF{
     reqS.htmlTolerance=2;
     reqS.exitWithErrorIfUnsafe=0;
     std::string fR= PTKF::GL_PLAINTEXT_KEEPER.recover(sanitizeForDisplay(t,reqS));
-    fR=MFRF::findAndReplace(fR,HCBF::verySafePlace,1,-1);
+    fR=MFRF::findAndReplace(fR,HCBF::verySafePlaceInv,1);
     return fR;
   }
   long checkForHTMLAwareness(const std::string &input){
